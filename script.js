a3e5a39c82f6de45b643860b5981682602195d1e
Cisca <233951544+CiscaSolana@users.noreply.github.com>
2025-12-11 19:25:37 +0100

    Build grid 16x16

---
+++ script.js
@@ -0,0 +1,14 @@
+//16x16 sono uguali a 256 boxes. Posso fare un unico ciclo for in cui faccio l'append di 256 boxes.
+//NOTA: andrò a settare la width del container come 100vw (cioe' il 100% della view width) mentre per ciascuna 
+//box andrò a impostare una width del 100% / 16 = 6.25% 
+
+//setto le caratteristiche del container della griglia 16x16
+const container = document.querySelector(".container");
+
+
+for(let i=0; i<256; i++){
+    //creo l'elemento <div> riga
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+}
